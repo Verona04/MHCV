bd06fde9ffc5a6fbce158b514c48d7b6a2249ac0
Carola17 <35689705+Carola17@users.noreply.github.com>
2018-02-13 23:58:13 +0100

    Oppgave 4c

--- Obligatorisk oppgave 1/ascii/iso_test.go
+++ Obligatorisk oppgave 1/ascii/iso_test.go
@@ -1,1 +1,12 @@
 package main
+
+import "testing"
+
+func TestExtendedASCIIText(t * testing.T) {
+	var testStr = "hæ"
+	testResult := ExtendedASCIIText(testStr)
+
+	if len(testResult) == 0 {
+		t.Fail()
+	}
+}
