94a6609110a754532e6f10925c912ee2ceb67152
Verona04 <35689729+Verona04@users.noreply.github.com>
2018-03-06 20:07:18 +0100

    fileinfo.go test1

--- Oblig2/src/Oppgave 1/fileinfo.go
+++ Oblig2/src/Oppgave 1/fileinfo.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"log"
+)
 
 func main() {
 
+	fi, err := os.Lstat("fileinfo.go")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Name: ", fi.Name())
+	fmt.Println("Size : ", fi.Size())
+	switch mode := fi.Mode(); {
+	case mode.IsDir():
+		fmt.Println("directory")
+	case mode.IsRegular():
+		fmt.Println("Regular file")
+		fmt.Println("UnixPermissionBits", mode.Perm())
+	case os.ModeAppend != 0:
+		fmt.Println("Is Append")
+	case os.ModeDevice != 0:
+		fmt.Println("Is a Device file")
+	case os.ModeSymlink != 0:
+		fmt.Println("Is a Symbolic Link")
+	}
 }
