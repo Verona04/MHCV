2580ae7994cd3dd5524d425925038bf0d5e661b6
Verona04 <35689729+Verona04@users.noreply.github.com>
2018-03-09 10:54:28 +0100

    Oppgave 3 og 4

--- Oblig2/src/Oppgave 3/addup.go
+++ Oblig2/src/Oppgave 3/addup.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+//	"time"
+)
+
 
 func main() {
+	c := make(chan int)
+	inputNumbers(c)
+}
 
+
+func inputNumbers(c chan int){
+	var a, b int
+	fmt.Println("Enter a number: ")
+
+	_, err := fmt.Scanf("%d", &a)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Enter another number: ")
+
+	fmt.Scanf("%d", &b)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("You entered the numbers ", a, "and ", b)
+	go func(a int, b int) { //fikk issues med fatal error: all goroutines are asleep - deadlock, så valgte å bruke en anonym func
+		c <- a
+		c <- b  }(a,b)
+
+	addNumbers(c)
+
+	res := <- c
+	fmt.Println("Sum numbers: ", res)
+
+}
+
+func addNumbers(c chan int){
+	a, b := <-c, <-c
+	res := (a+b)
+
+	go func() { c <- res }()
 }
